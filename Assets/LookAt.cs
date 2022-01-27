113b864756897a7170f5f427a01b39c07b17732d
ShandonFleming948 <shandonflem@gmail.com>
2022-01-27 18:02:04 -0500

    Setup cylinder to follow the player

--- Assets/LookAt.cs
+++ Assets/LookAt.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
+public class LookAt : MonoBehaviour
 {
 
-  public float rotationSpeed = 0.1f;
+  public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+        this.transform.LookAt( target.transform );
     }
 }
